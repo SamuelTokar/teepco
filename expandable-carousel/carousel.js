e6c659d2f42c7f933de36a37ce4abb00c721869e
Adam Doležal <dolezal.adam@merkatos.cz>
2024-04-21 23:48:30 +0200

    feat: new expando.. carousel

---
+++ expandable-carousel/carousel.js
@@ -0,0 +1,23 @@
+$(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true,
+    center: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
+  
